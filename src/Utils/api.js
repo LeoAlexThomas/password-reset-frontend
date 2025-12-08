d1227914519feac1df31384a782e1e416c063860
LeoAlexThomas <leoalex960@gmail.com>
2025-12-08 19:48:09 +0530

    updated backend url

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://password-reset-backend-rby7.onrender.com/api",
 });
 
 api.interceptors.request.use(
